73e086232930971b63570767705df661371b7139
Emu Kamada <es377233@gmail.com>
2023-10-08 14:16:06 +0900

    quote syntax error追加

--- tokenizer/syntax_error_handling.c
+++ tokenizer/syntax_error_handling.c
@@ -3,7 +3,30 @@
 #include <stdbool.h>
 #include <libc.h>
 
-
+int quote_error(t_token *tokens)
+{
+	int i = 0;
+	int j;
+	char quote_char;
+	while (tokens[i].arg)
+	{
+		j = 0;
+		if (tokens[i].arg[0] == '\'' || tokens[i].arg[0] == '\"')
+		{
+			quote_char = tokens[i].arg[0];
+			while (tokens[i].arg[j])
+				j++;
+			j--;
+			if (tokens[i].arg[j] != quote_char || j == 0)
+			{
+				printf("minishell: syntax error: unexpected end of file\n");
+				return 1;
+			}
+		}
+		i++;
+	}
+	return 0;
+}
 
 int pipe_error(t_token *tokens)
 {
@@ -36,6 +59,8 @@ int pipe_error(t_token *tokens)
 int syntax_error(t_token *tokens)
 {
 	if (pipe_error(tokens))
+		return 1;
+	if (quote_error(tokens))
 		return 1;
 	return 0;
 }
