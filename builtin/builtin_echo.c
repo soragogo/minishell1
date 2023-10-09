e61f6cd869bc4fb6937387e3be5abb550b2a2b50
Emu Kamada <es377233@gmail.com>
2023-10-09 16:57:33 +0900

    echo -nオプション正しく作動するようにした

--- builtin/builtin_echo.c
+++ builtin/builtin_echo.c
@@ -1,24 +1,44 @@
 #include "./../includes/minishell.h"
 
-void ft_putstr(char *ch, int fd)
+void	ft_putstr(char *ch, int fd)
 {
 	if (!ch)
-		return;
+		return ;
 	write(fd, ch, strlen(ch));
 }
 
-int ft_echo(char **command, int status)
+int	handle_n_option(char **command, int *i)
 {
-	int i;
-	int n_option;
+	int		n_option;
+	char	*ptr;
 
-	i = 1;
 	n_option = 0;
-	if (command[i] && ft_strncmp(command[i], "-n", 3) == 0)// && strlen(command[i]) == 2)
+	while (command[*i])
 	{
-		n_option = 1;
-		i++;
+		if (ft_strncmp(command[*i], "-n", 2) == 0)
+		{
+			ptr = command[*i] + 2;
+			while (*ptr == 'n')
+				ptr++;
+			if (*ptr == '\0')
+			{
+				n_option = 1;
+				(*i)++;
+				continue ;
+			}
+		}
+		break ;
 	}
+	return (n_option);
+}
+
+void	output_command(char **command, int start_idx, int status)
+{
+	int	i;
+
+	i = start_idx;
+	if (command[i] && !ft_strncmp(command[i], "-", 2))
+		i++;
 	while (command[i])
 	{
 		if (command[i] && ft_strncmp(command[i], "$?", 2) == 0)
@@ -29,6 +49,16 @@ int ft_echo(char **command, int status)
 		if (command[i])
 			write(1, " ", STDOUT_FILENO);
 	}
+}
+
+int	ft_echo(char **command, int status)
+{
+	int	i;
+	int	n_option;
+
+	i = 1;
+	n_option = handle_n_option(command, &i);
+	output_command(command, i, status);
 	if (n_option == 0)
 		write(1, "\n", STDOUT_FILENO);
 	return (0);
