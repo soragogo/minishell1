869e2600ffb4679acdffeb58f930e7b72728cce4
Emu Kamada <es377233@gmail.com>
2023-10-11 21:51:37 +0900

    tester修正

--- tester/test.sh
+++ tester/test.sh
@@ -8,12 +8,37 @@ function run_test() {
         printf "\e[32mOK\e[0m\n"
     else
         printf "\e[31mKO\e[0m\n"
-        printf "\e[31mTested: $1\e[0m\n"
-		printf "\e[31mExpected: $2\e[0m\n"
-		printf "Got: $output\n"
+        printf "      \e[31mTested: [$1]\e[0m\n"
+		printf "      \e[31mExpected: [$2]\e[0m\n"
+		printf "      \e[31mGot: [$output]\e[0m\n"
         # echo "--------------------------------"
     fi
 }
+
+run_error_test() {
+    local command="$1"
+    local expected_stdout="$2"
+    local expected_stderr="$3"
+
+    # コマンドを実行して、標準出力と標準エラー出力の両方をキャッチします
+    local actual_stdout=$({ echo "$command" | ./a.out; } 2>./tmp.stderr)
+    local actual_stderr=$(cat ./tmp.stderr)
+
+    # 出力を比較します
+    if [[ "$actual_stdout" == "$expected_stdout" && "$actual_stderr" == "$expected_stderr" ]]; then
+        printf "\e[32mOK\e[0m\n"
+    else
+        printf "\e[31mKO\e[0m\n"
+        printf "      \e[31mTested: $command\e[0m\n"
+        printf "      \e[31mExpected stdout: $expected_stdout\e[0m\n"
+        printf "      \e[31mGot stdout: $actual_stdout\e[0m\n"
+        printf "      \e[31mExpected stderr: $expected_stderr\e[0m\n"
+        printf "      \e[31mGot stderr: $actual_stderr\e[0m\n"
+    fi
+}
+
+# 使用例
+
 
 # コンパイル
 cd ../
@@ -21,10 +46,12 @@ cd ../
 cc exec/*.c tokenizer/*.c builtin/*.c libft/*.c utils/*.c main.c -lreadline
 # テストケースの実行
 
-run_test "echo hello" "hello"
-run_test "ls" "$(ls)"
-run_test "       ls" "$(ls)"
-run_test "ls -l | grep -v \"c\"" "$(ls -l | grep -v "c")"
+run_error_test "ls non_existent_file" "" "ls: non_existent_file: No such file or directory"
+run_test "echo hello" "hello" ""
+run_test "ls" "$(ls)" ""
+run_test "       ls" "$(ls)" ""
+run_test "ls -l | grep -v \"c\"" "$(ls -l | grep -v "c")" ""
+
 
 # ... 他のテストケースを追加 ...
 echo "--------------------------------"
@@ -75,9 +102,9 @@ printf  "\e[33mENV EXPANSIONS + ESCAPE TESTS\e[0m\n"
 # run_test 'echo test     \    test' "$(echo test     \    test)"
 # run_test 'echo \"test' "$(echo \"test)"
 run_test 'echo $TEST' "$(echo $TEST)"
-run_test 'echo "$TEST"' ""
+run_test 'echo "$TEST"' "$(echo "$TEST")"
 run_test "echo '$TEST'" "$TEST"
-run_test 'echo "$TEST$TEST$TEST"' "$(echo $TEST)"
+run_test 'echo "$TEST$TEST$TEST"' "$(echo "$TEST$TEST$TEST")"
 run_test 'echo "$TEST$TEST=lol$TEST"' "$(echo "$TEST$TEST=lol$TEST")"
 run_test 'echo "   $TEST lol $TEST"' "$(echo "   $TEST lol $TEST")"
 run_test 'echo $TEST$TEST$TEST' "$(echo $TEST$TEST$TEST)"
@@ -94,7 +121,7 @@ run_test 'echo "$T1TEST"' "$(echo "$T1TEST")"
 
 echo "--------------------------------"
 # # ENV EXPANSIONS
-# printf  "\e[33mENV EXPANSIONS TESTS\e[0m\n"
+printf  "\e[33mENV EXPANSIONS TESTS\e[0m\n"
 # ENV_SHOW="env | sort | grep -v SHLVL | grep -v _="
 # EXPORT_SHOW="export | sort | grep -v SHLVL | grep -v _= | grep -v OLDPWD"
 # run_test 'export =' "$(export =)"
@@ -110,43 +137,54 @@ echo "--------------------------------"
 # run_test $EXPORT_SHOW "$($EXPORT_SHOW)"
 # run_test 'export TEST="ls       -l     - a" ; echo $TEST ; $LS ; ' $ENV_SHOW "$('export TEST="ls       -l     - a" ; echo $TEST ; $LS ; ' $ENV_SHOW)"
 
-echo "--------------------------------"
-# # REDIRECTIONS
-printf  "\e[33mREDIRECTIONS TESTS\e[0m\n"
-run_test 'echo test > ls ; cat ls' "$(echo test > ls ; cat ls)"
-run_test 'echo test > ls >> ls >> ls ; echo test >> ls; cat ls' "$(echo test > ls >> ls >> ls ; echo test >> ls; cat ls)"
-run_test '> lol echo test lol; cat lol' "$(> lol echo test lol; cat lol)"
-run_test '>lol echo > test>lol>test>>lol>test mdr >lol test >test; cat test' "$(>lol echo > test>lol>test>>lol>test mdr >lol test >test; cat test)"
-run_test 'cat < ls' "$(cat < ls)"
-run_test 'cat < ls > ls' "$(cat < ls > ls)"
 
 echo "--------------------------------"
-# # SYNTAX ERROR
-# printf  "\e[33mSYNTAX ERROR TESTS\e[0m\n"
-# run_test '| test' "minishell: syntax error near unexpected token `|'"
-# run_test 'echo > <' "minishell: syntax error near unexpected token `<'"
-# run_test 'echo | |' "minishell: syntax error near unexpected token `|'"
-# run_test '<' "minishell: syntax error near unexpected token `newline'"
+# SYNTAX ERROR
+printf  "\e[33mSYNTAX ERROR TESTS\e[0m\n"
+run_error_test '| test' "" "minishell: syntax error near unexpected token `|'"
+run_error_test 'echo > <' "" "minishell: syntax error near unexpected token `<'"
+run_error_test 'echo | |' "" "minishell: syntax error near unexpected token `|'"
+run_error_test '<' "" "minishell: syntax error near unexpected token `newline'"
 
 # # EXIT
 printf  "\e[33mEXIT ERROR TESTS\e[0m\n"
 run_test "exit 42" "$(exit 42)"
-run_test "exit 42 53 68" "exit
-minishell: exit: too many arguments"
+run_error_test "exit 42 53 68" "" "minishell: exit: too many arguments"
 run_test "exit 259" "$(exit 259)"
-run_test "exit 9223372036854775807" "$(exit 9223372036854775807)"
-run_test "exit -9223372036854775808" "$(exit -9223372036854775808)"
-run_test "exit 9223372036854775808" "$(exit 9223372036854775808)"
-run_test "exit -9223372036854775810" "$(exit -9223372036854775810)"
+run_error_test "exit 9223372036854775807" "$(exit 9223372036854775807)"
+run_error_test "exit -9223372036854775808" "$(exit -9223372036854775808)"
+# run_error_test "exit 9223372036854775808" "$(exit 9223372036854775808)" "minishell: exit: 9223372036854775808: numeric argument required"
+# run_error_test "exit -9223372036854775810" "$(exit -9223372036854775810)" "minishell: exit: -9223372036854775810: numeric argument required"
 run_test "exit -4" "$(exit -4)"
-run_test "exit wrong" "$(exit wrong)"
-run_test "exit wrong_command" "$(exit wrong_command)"
-run_test "gdagadgag" "$(gdagadgag)"
-run_test "ls -Z" "$(ls -Z)"
-run_test "cd gdhahahad" "minishell: cd: gdhahahad: No such file or directory"
-run_test "ls -la | wtf" "minishell: wtf: command not found"
-
-rm lol ls test
+run_error_test "exit wrong" "$(exit wrong)" "minishell: exit: wrong: numeric argument required"
+run_error_test "exit wrong_command" "$(exit wrong_command)" "minishell: exit: wrong_command: numeric argument required"
 
 
 
+echo "--------------------------------"
+# # EXIT
+printf  "\e[33mMULTI ERROR TESTS\e[0m\n"
+run_error_test "gdagadgag" "$(gdagadgag)" "minishell: gdagadgag: command not found"
+run_error_test "ls -Z" "$(ls -Z)" "ls: invalid option -- Z
+usage: ls [-@ABCFGHILOPRSTUWabcdefghiklmnopqrstuvwxy1%,] [--color=when] [-D format] [file ...]"
+run_error_test "cd gdhahahad" "" "minishell: cd: gdhahahad: No such file or directory"
+run_error_test "ls -la | wtf" "" "minishell: wtf: command not found"
+run_error_test "cd nonexistent_directory" "" "minishell: cd: nonexistent_directory: No such file or directory"
+run_error_test "ls --no-option" "" "ls: unrecognized option '--no-option'"
+
+
+
+
+# echo "--------------------------------"
+# # # REDIRECTIONS
+# printf  "\e[33mREDIRECTIONS TESTS\e[0m\n"
+# run_test 'echo test > ls | cat ls' "$(echo test > ls | cat ls)"
+# run_test 'echo test > ls >> ls >> ls | echo test >> ls | cat ls' "$(echo test > ls >> ls >> ls | echo test >> ls | cat ls)"
+# run_test '> lol echo test lol | cat lol' "$(> lol echo test lol | cat lol)"
+# run_test '>lol echo > test>lol>test>>lol>test mdr >lol test >test | cat test' "$(>lol echo > test>lol>test>>lol>test mdr >lol test >test | cat test)"
+# run_test 'cat < ls' "$(cat < ls)"
+# run_test 'cat < ls > ls' "$(cat < ls > ls)"
+
+# rm -f ls lol test 'ls;' 'test;'
+
+rm tmp.stderr ./a.out
