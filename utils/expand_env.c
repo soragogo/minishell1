3e862ffe87e6f595c7e0426c7fc7fcf0dabaa4bb
Emu Kamada <es377233@gmail.com>
2023-10-15 21:47:47 +0900

    refactor.今のとこ動いてる

--- utils/expand_env.c
+++ utils/expand_env.c
@@ -3,17 +3,15 @@
 
 
 char *deal_env(char *arg, int *i, t_env *env_head, int *increment)
-{puts("-----deal_env-----");
+{
     char *start;
     char *env_value;
     char *expanded;
 
     env_value = NULL;
     expanded = NULL;
-    puts("[1]");
     (*i)++;
     start = &arg[*i];
-    printf("start: [%s]\n", start);
     if (ft_isdigit(arg[*i]))
     {
         while (ft_isdigit(arg[*i]))
@@ -23,28 +21,11 @@ char *deal_env(char *arg, int *i, t_env *env_head, int *increment)
         while (arg[*i] && (ft_isalnum(arg[*i]) || arg[*i] == '_'))
         (*i)++;
     }
-    // printf("&arg[%d] - start: [%ld]\n", *i, &arg[*i] - start);
     env_value = ft_substr(start, 0, &arg[*i] - start);
-    // (*i)++;
     start = &arg[*i];
-    printf("start: [%s]\n", start);
-
     expanded = map_get(&env_head, env_value);
-
-    // if (tmp)
-    // {
-    //     joined = ft_strjoin(tmp, expanded);
-    //     // free(tmp);
-    // }
-    // else
-    //     joined = ft_strdup(expanded);
     *increment += ft_strlen(expanded);
     *increment -= (ft_strlen(env_value)+1);
-    printf("ft_strlen(expanded)%zu\n",ft_strlen(expanded));
-    printf("ft_strlen(env_value)%zu\n",ft_strlen(env_value));
-    printf("expanded: %s\n", expanded);
-    printf("env_value: %s\n", env_value);
-    puts("----------");
     if (env_value)
     {
         free(env_value);
@@ -67,67 +48,34 @@ char *deal_raw_env(char *arg, int *i, t_env *env_head)
     rest = NULL;
     expanded = NULL;
     tmp = ft_substr(arg, 0, *i);
-    printf("tmp:[%s]\n",tmp);
-    // (*i)++;
-    rest = &arg[*i];
+    rest = &arg[*i + 1];
     rest++;
     if (ft_isdigit(*rest))
     {
         while (ft_isdigit(*rest))
             rest++;
     }
-    else{
+    else
+    {
         while (*rest && (ft_isalnum(*rest) || *rest == '_'))
         rest++;
     }
-    if (*rest)
-        rest = ft_strdup(rest);
-    else
-        rest = NULL;
-    printf("rest:[%s]\n",rest);
-
+    rest = ft_strdup(rest);
     expanded = deal_env(arg, i, env_head, &increment);
-    printf("expanded: [%s]\n", expanded);
-    if (!tmp)
-        joined = ft_strdup(expanded);
-    else
-    {
-        joined = ft_strjoin(tmp, expanded);
-    }
-    if (tmp)
-    {
-        free(tmp);
-        tmp = NULL;
-    }
-    printf("joined: [%s]\n", joined);
-    if (!rest)
-        tmp = ft_strdup(joined);
-    else
-        tmp = ft_strjoin(joined, rest);
+    joined = ft_strjoin(tmp, expanded);
+    free(tmp);
+    tmp = NULL;
+    tmp = ft_strjoin(joined, rest);
     free(rest);
     free(joined);
     free(expanded);
-    rest = NULL;
-    joined = NULL;
-    expanded = NULL;
     (*i) += increment;
-    printf("%d\n", *i);
-    printf("tmp: %s\n", tmp);
-
-    // if (joined)
-        // free(joined);
-    // if(expanded)
-        // free(expanded);
-    // if(rest)
-        // free(rest);
-    // *i += increment + 2;
     free(arg);
     return (tmp);
 }
 
 char *expand_env(char *arg, int *i, t_env *env_head, int *increment)
 {
-    puts("------------expand_env--------------");
     char *start;
     char *env_value;
     char *expanded;
@@ -135,9 +83,6 @@ char *expand_env(char *arg, int *i, t_env *env_head, int *increment)
     char *joined;
 
     start = &arg[*i];
-    printf("start: [%s]\n", start);
-
-
     tmp = NULL;
     joined = NULL;
     expanded = NULL;
@@ -146,7 +91,6 @@ char *expand_env(char *arg, int *i, t_env *env_head, int *increment)
         if (arg[*i] == '$')
         {
             expanded = deal_env(arg, i, env_head, increment);
-            printf("%d increment later\n", *increment);
             start++;
             if (ft_isdigit(*start))
             {
@@ -157,54 +101,22 @@ char *expand_env(char *arg, int *i, t_env *env_head, int *increment)
                 while (*start && (ft_isalnum(*start) || *start == '_'))
                 start++;
             }
-            // start++;
-            printf(":) start :%s\n", start);
-            printf("joined [%s]\n", joined);
         }
         else
         {
-            puts("[2]");
             while (arg[*i] && arg[*i] != '\"' && arg[*i] != '$')
                 (*i)++;
-            printf("&arg[%d] - start: [%ld]\n", *i, &arg[*i] - start);
             expanded = ft_substr(start, 0, &arg[*i] - start);
-            printf("expanded [%s]\n", expanded);
         }
-        // if (!joined)
-        // {
-        puts("[3]");
         tmp = ft_strdup(joined);
-        // if (joined)
-            // free(joined);
         free(joined);
         joined = NULL;
         joined = ft_strjoin(tmp, expanded);
-        // if (tmp)
-            // free(tmp);
-        // if (expanded)
-            // free(expanded);
-        // }
-        // else{
-        //     puts("[4]");
-        //     printf("joined [%s]\n", joined);
-        //     tmp = ft_strdup(joined);
-        //     joined = ft_strjoin(tmp,expanded);
-
-        // }
         free(tmp);
         tmp = NULL;
         free(expanded);
         expanded = NULL;
-        printf("joined [%s]\n", joined);
-        printf("start [%s]\n", start);
-        printf("arg [%s]\n", &arg[*i]);
     }
     (*i)++;
-    printf("joined [%s]\n", joined);
-    puts("-----------------------------");
-    // if (expanded)
-    //     free(expanded);
-    // if(arg)
-    //     free(arg);
     return (joined);
 }
--- utils/quote.c
+++ utils/quote.c
@@ -5,75 +5,6 @@
 char *deal_env(char *arg, int *i, t_env *env_head);
 char *deal_raw_env(char *arg, int *i, t_env *env_head);
 
-int remove_quorts(char **command, char bracket, int start)
-{
-    int len;
-    int i;
-    char *tmp;
-
-    len = 0;
-    len = ft_strlen(command[0]);
-    i = 0;
-    while (command[0][i + 1] != bracket && command[0][i] != '\0')//ここなんで＋１？
-        i++;
-    if (command[0][i] == '\0')
-        return (start + i + 1);
-    tmp = ft_substr(command[0], 1, len - 2);
-    ft_strlcpy(command[0], tmp, len - 1);
-    // free(tmp);
-    return (start + i + 2);
-}
-
-// void expand_quote(char **command, t_env *env_head)
-// {
-//     int i;
-//     int j;
-//     char *tmp;
-//     char *before_env;
-//     char *env_value;
-//     char quort;
-
-//     i = 0;
-//     j = 1;
-//     tmp = NULL;
-//     if (command[0] == NULL)
-//         return ;
-//     // i = skip_space(command[0]);
-//     command[0] = &command[0][i];
-//     // ' "を探す
-//     while (command[0][i] != '\'' && command[0][i] != '\"' && command[0][i] != '\0')
-//         i++;
-//     if (command[0][i] == '\'')
-//         quort = '\'';
-//     else if (command[0][i] == '\"')
-//         quort = '\"';
-//     //後の' "を探す
-//     while (command[0][i + j] != quort && command[0][i + j] != '\0')
-//         j++;
-//     // ' "があったら
-//     if (command[0][i + 1] != '\0')
-//     {
-//         before_env = ft_substr(command[0], 0, i);
-//         tmp = ft_substr(command[0], i, j + 1);
-//         i = remove_quorts(&tmp, quort, i);
-//         if (quort == '\"')
-//         {
-//             expand_env(&tmp, env_head);
-//         }
-//         before_env = ft_strjoin(before_env, tmp);
-//         if (command[0][i] != '\0'){
-//             tmp = ft_substr(command[0], i, ft_strlen(command[0]));
-//             expand_quote(&tmp, env_head);
-//             before_env = ft_strjoin(before_env, tmp);
-//             // tmp = ft_strjoin(tmp, &command[0][i]);
-//         }
-//         // command[0] = ft_strdup(tmp);
-//         command[0] = ft_strdup(before_env);
-//         free(tmp);
-//     }else{
-//         expand_env(command, env_head);
-//     }
-// }
 
 void deal_single_quote(char *arg, int *i)
 {
@@ -103,65 +34,29 @@ void deal_single_quote(char *arg, int *i)
 
 char *deal_double_quote(char *arg, int *i, t_env *env_head)
 {
-    puts("--------------deal_double_quote---------------");
     char *rest;
     char *expanded;
     char *tmp;
     char *joined;
     int increment;
 
-    rest = NULL;
     increment = 0;
+    rest = NULL;
     expanded = NULL;
     tmp = NULL;
     while (arg[(*i)] && arg[(*i)] != '\"')
         (*i)++;
-    tmp = ft_substr(arg, 0, &arg[(*i)] - arg);
-    printf("tmp [%s]\n", tmp);
+    tmp = ft_substr(arg, 0, *i);
     (*i)++;
     expanded = expand_env(arg, i, env_head, &increment);
-    printf("i: %d\n", *i);
-    // while (arg[(*i)] && arg[(*i)] != '\"')
-    //     (*i)++;
-    // (*i)++;
-    // return (NULL);
     if (arg[(*i)] != '\0')
         rest = &arg[(*i)];
-    printf("rest [%s]\n", rest);
-    if (!expanded)
-        joined = ft_strdup(tmp);
-    else {
-        joined = ft_strjoin(tmp, expanded);
-        // free(expanded);
-    }
-    printf("joined [%s]\n", joined);
-    *i -= 2;
-    *i += increment;
-    // free(tmp);
-    if (tmp)
-    {
-        free(tmp);
-        tmp = NULL;
-    }
-    if (rest != NULL)
-    {
-        // tmp = ft_strdup(joined);
-        printf("tmp [%s]\n", tmp); // => tmp [/Users/emukamada]
-        printf("joined: [%s]\n", joined); // => joined: []
-        tmp = ft_strjoin(joined, rest);
-        printf("tmp: [%s]\n", tmp); // => (ft_strjoin) joined: [s]
-    }
-    else
-    {
-        tmp = ft_strdup(joined);
-    }
+    *i += increment - 2;
+    joined = ft_strjoin(tmp, expanded);
+    free(tmp);
+    tmp = ft_strjoin(joined, rest);
     free(joined);
     free(expanded);
-    joined = NULL;
-    expanded = NULL;
-    printf("tmp: [%s]\n", tmp); // => joined: [s]
-
-    // arg = joined;
     free(arg);
     return tmp;
 }
@@ -184,22 +79,14 @@ char *expand_quote(char *arg, t_env *env_head)
         if (arg[i] == '\'')
         {
             deal_single_quote(arg, &i);
-            printf("result: [%s]\n", arg);
-            printf("current i position: [%d]\n", i);
         }
         else if (arg[i] == '\"')
         {
-            // return (arg);
             arg = deal_double_quote(arg, &i, env_head);
-            printf("result: [%s]\n", arg);
-            printf("current i position: [%d]\n", i);
         }
         else if (arg[i] == '$')
         {
-            // expand_env(arg, &i, env_head, &increment);
             arg = deal_raw_env(arg, &i, env_head);
-            printf("result: [%s]\n", arg);
-            printf("current i position: [%d]\n", i);
         }
     }
     return (arg);
@@ -207,29 +94,3 @@ char *expand_quote(char *arg, t_env *env_head)
 
 
 
-/* -------------------------------------------------------------------------- */
-
-// void fatal_error(char *msg)
-// {
-//     ft_putstr_fd("minishell: ", STDERR_FILENO);
-//     ft_putendl_fd(msg, STDERR_FILENO);
-// 	exit(1);
-// }
-
-// int main() {
-//     char *command = NULL; // テスト対象のコマンド文字列
-//     int flag = 0; // シングルクォートフラグ（0: シングルクォート外、1: シングルクォート内）
-//     t_env *env_head = NULL; // 環境変数リストの先頭アドレス
-//     envmap_init(&env_head);
-// 	// info.map_head = env;
-//     printf("%s\n", map_get(&env_head, "HOME"));
-
-//     // テストケース1: シングルクォート外の置換
-//     command = strdup("\"\'$HOME\'\"$HOME \"\'$HOME\'\"$HOME");
-//     expand_quote(&command, env_head);
-//     printf("テストケース1: %s\n", command);
-//     free(command);
-//     command = NULL;
-
-//     return 0;
-// }
