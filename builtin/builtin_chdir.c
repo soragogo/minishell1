f9521e7676a33ae4858da942e520ab5892c6589d
Mayu Yamada <mr.obake0208@yahoo.com>
2023-10-18 01:46:47 +0900

    leak naoshi

--- builtin/builtin_chdir.c
+++ builtin/builtin_chdir.c
@@ -1,43 +1,85 @@
 #include "./../includes/minishell.h"
 
-void delete_path(char *dir_path)
+char *delete_path(char *dir_path)
 {
 	int i;
+    char *ret;
 
     if(!dir_path)
-        return ;
+        return (NULL);
 	i = ft_strlen(dir_path);
 	while (i > 2 && dir_path[i - 1] != '/')
 		i--;
-	if (i != 0)
-		dir_path[i - 1] = '\0';
+	if (i != 0){
+        ret = malloc(sizeof(char) * i + 1);
+        if (!ret)
+            fatal_error("malloc error");
+        ft_strlcpy(ret, dir_path, i);
+    }
+		// dir_path[i - 1] = '\0';
 	else
-		dir_path[i] = '\0';
+    {
+        ret = malloc(sizeof(char) * i + 1);
+        if (!ret)
+            fatal_error("malloc error");
+        ft_strlcpy(ret, dir_path, i);
+    }
+		// dir_path[i] = '\0';
+    // ret = ft_strdup(dir_path);
+    free(dir_path);
+    return (ret);
 }
 
-char *convert_relative_path(char **dir_path, char *input)
+char *join_path(char *dir_path, char *input)
 {
+    char *tmp;
+
+    tmp = ft_strjoin(dir_path, "/");
+    free(dir_path);
+    dir_path = ft_strjoin(tmp, input);
+    free(tmp);
+    return (dir_path);
+}
+
+void free_split(char **tmp)
+{
+    int i;
+
+    i = 0;
+    while (tmp[i])
+    {
+        free(tmp[i]);
+        i++;
+    }
+    free(tmp);
+}
+
+char *convert_relative_path(char *dir_path, char *input)
+{
+    int i;
 	char **tmp = ft_split(input, '/');
     char *ret;
 
-	while (*tmp)
+    i = 0;
+	while (tmp[i])
 	{
-		if (ft_strncmp(*tmp, "..", 3) == 0)
+		if (ft_strncmp(tmp[i], "..", 3) == 0)
 		{
-			delete_path(*dir_path);
-            printf("dir_path: %s\n", *dir_path);
+			dir_path = delete_path(dir_path);
+            printf("dir_path: %s\n", dir_path);
 		}
 		else if (ft_strncmp(*tmp, ".", 2) == 0)
 			;
 		else
 		{
-			ft_strlcat(*dir_path, "/", PATH_MAX);
-			ft_strlcat(*dir_path, *tmp, PATH_MAX);
+            dir_path = join_path(dir_path, tmp[i]);
 		}
-		tmp++;
+		i++;
 	}
-    ret = ft_strdup(*dir_path);
-    return (ret);
+    // ret = ft_strdup(*dir_path);
+    printf("dir_path: %s\n", dir_path);
+    free_split(tmp);
+    return (dir_path);
 }
 
 void init_dir_path(char **home, char **pwd_path, char **old_path, t_env **env){
@@ -64,40 +106,32 @@ int ft_chdir(char **commands, t_env **env)
     char *pwd_path;
 	char *old_pwd = NULL;
 	char *dir_path = NULL;
+
     init_dir_path(&home, &pwd_path, &old_pwd, env);
     printf("home: %s\n", home);
     printf("pwd_path: %s\n", pwd_path);
     printf("old_pwd: %s\n", old_pwd);
-
-/* ------------------------------------------------------------- */
 	if (commands[1] == NULL)//引数がない場合
-		// ft_strlcpy(dir_path, home, PATH_MAX);
         dir_path = ft_strdup(home);
 	else if (ft_strncmp(commands[1], "~", 2) == 0)//引数が~の場合
 	{
-		// ft_strlcpy(dir_path, home, PATH_MAX);
-		// ft_strlcat(dir_path, commands[1] + 1, PATH_MAX);
         dir_path = ft_strjoin(home, commands[1] + 1);
 	}
-	// else if (commands[1] == '-')//引数が-の場合
 	else if (ft_strncmp(commands[1], "-", 2) == 0)//引数が-の場合
 	{
         dir_path = ft_strdup(old_pwd);
-		// dir_path = map_get(env, "OLDPWD");
 		if (!dir_path){
 			error_message("cd", NULL, "OLDPWD not set");
 			return (1);
 		}
-		// printf("%s\n", dir_path);
 	}
 	else//ディレクトリ直書きの場合
 	{
 		if (ft_strncmp(commands[1], "/", 2) == 0)//絶対パスの場合
-			// ft_strlcpy(dir_path, commands[1], PATH_MAX);
             dir_path = ft_strdup(commands[1]);
 		else
 		{
-			dir_path = convert_relative_path(&pwd_path, commands[1]);
+			dir_path = convert_relative_path(ft_strdup(pwd_path), commands[1]);
 		}
 	}
 	if (chdir(dir_path) != 0)
@@ -111,15 +145,7 @@ int ft_chdir(char **commands, t_env **env)
 		free(dir_path);
 		return (1);
 	}
-    // printf("dirpath_pointer2: %p\n", dir_path);
-    // printf("oldpwd_pointer2: %p\n", old_pwd);
-	// printf("OLDPWD: %s\n\n", map_get(env, "OLDPWD"));
-	// printf("PWD: %s\n", map_get(env, "PWD"));
-	// printf("%s\n", dir_path);
 	ft_pwd();//確認用
-	// free(old_pwd);
     free(dir_path);
-    // free(home);
-    // free(pwd_path);
 	return (0);
 }
