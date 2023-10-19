90e4c09b6e11324dac82860d5c03de65fbe547d8
Mayu Yamada <mr.obake0208@yahoo.com>
2023-10-19 17:56:54 +0900

    norm

--- includes/parser.h
+++ includes/parser.h
@@ -3,55 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emukamada <emukamada@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mayu <mayu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 19:38:34 by ekamada           #+#    #+#             */
-/*   Updated: 2023/10/18 18:44:45 by emukamada        ###   ########.fr       */
+/*   Updated: 2023/10/19 17:55:08 by mayu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
-#define PARSER_H
+# define PARSER_H
 
-#include <libc.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <string.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
-#include "token.h"
-#include "../libft/libft.h"
+# include <libc.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include "token.h"
+# include "../libft/libft.h"
 
 typedef struct s_node
 {
-	int newfd;
-	int stashfd;
-	int oldfd;
-	TYPE type;
-	const char *filename;
-	struct s_node *next;
-	struct s_node *prev;
-} t_redirect;
+	int				newfd;
+	int				stashfd;
+	int				oldfd;
+	t_type			type;
+	const char		*filename;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_redirect;
 
 typedef struct s_commandset
 {
-	t_redirect *node;
-	char **command;
-	pid_t pid;
-	struct s_commandset *next;
-	struct s_commandset *prev;
-} t_commandset;
+	t_redirect			*node;
+	char				**command;
+	pid_t				pid;
+	struct s_commandset	*next;
+	struct s_commandset	*prev;
+}	t_commandset;
 
-void import_redirection(t_token *tokens, t_commandset *commandsets, int num_of_commands);
-void test_commandsets(t_commandset *commandsets, int num_of_commands);
-int syntax_error(t_token *tokens);
-void free_commandset(t_commandset *csets);
-t_token *ft_tokenizer(char *command);
-char *skip_spaces(char *str);
-int is_dilimeter(char c);
+void	import_redirection(t_token *tokens,
+			t_commandset *commandsets, int num_of_commands);
+void	test_commandsets(t_commandset *commandsets, int num_of_commands);
+int		syntax_error(t_token *tokens);
+void	free_commandset(t_commandset *csets);
+t_token	*ft_tokenizer(char *command);
+char	*skip_spaces(char *str);
+int		is_dilimeter(char c);
 void	split_into_tokens(t_token *tokens, char *command, int num_of_tokens);
 #endif
--- includes/token.h
+++ includes/token.h
@@ -1,41 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayu <mayu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 17:51:41 by mayu              #+#    #+#             */
+/*   Updated: 2023/10/19 17:54:59 by mayu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef TOKEN_H
-#define TOKEN_H
+# define TOKEN_H
 
-#include <libc.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <string.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
+# include <libc.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
 
-typedef enum
+typedef enum type
 {
 	UNCATEGORIZED,
-	PIPE,		   // | (パイプ)
-	REDIRECT_OUT,  // > (リダイレクト出力)
-	REDIRECT_IN,   // < (リダイレクト入力)
-	APPEND_OUT,	   // >> (出力の追加)
-	HERE_DOCUMENT, // << (ヒアドキュメント)
+	PIPE,
+	REDIRECT_OUT,
+	REDIRECT_IN,
+	APPEND_OUT,
+	HERE_DOCUMENT,
 	COMMAND,
 	COMMAND_OPTION,
 	FILE_NAME
-} TYPE;
+}	t_type;
 
 typedef struct s_list_token
 {
-	char *arg;
-	TYPE type;
-	int is_freed;
-	struct s_list_token *next_token;
+	char				*arg;
+	t_type				type;
+	int					is_freed;
+	struct s_list_token	*next_token;
+}	t_token;
 
-} t_token;
-
-t_token *ft_tokenizer(char *command);
-char *skip_spaces(char *str);
-int is_dilimeter(char c);
+t_token	*ft_tokenizer(char *command);
+char	*skip_spaces(char *str);
+int		is_dilimeter(char c);
 void	split_into_tokens(t_token *tokens, char *command, int num_of_tokens);
+
 #endif
