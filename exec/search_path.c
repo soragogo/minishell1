3de76805dd0a4f4c81ec1a3aff9a28060bbcdbda
Mayu Yamada <mr.obake0208@yahoo.com>
2023-10-19 16:14:11 +0900

    norminette main, error, searchpath

--- exec/search_path.c
+++ exec/search_path.c
@@ -6,51 +6,50 @@
 /*   By: mayu <mayu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:37:58 by mayu              #+#    #+#             */
-/*   Updated: 2023/10/18 15:38:20 by mayu             ###   ########.fr       */
+/*   Updated: 2023/10/19 16:03:12 by mayu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libc.h>
 #include "../includes/minishell.h"
 
-char	*fetch_path(char *file, t_env **map)
+char	*split_path(char *file, char *start, char *end, int *flag)
 {
 	char	path[PATH_MAX];
-	char	*pathlist;
+
+	ft_bzero(path, PATH_MAX);
+	if (end)
+		ft_strlcpy(path, start, end - start + 1);
+	else
+	{
+		*flag = 1;
+		ft_strlcpy(path, start, ft_strlen(start) + 1);
+	}
+	ft_strlcat(path, "/", PATH_MAX);
+	ft_strlcat(path, file, PATH_MAX);
+	if (access(path, F_OK) == 0)
+		return (ft_strdup(path));
+	return (NULL);
+}
+
+char	*fetch_path(char *file, t_env **map)
+{
 	char	*start;
 	char	*end;
-	char	*result;
 	int		flag;
-	int		len;
 
 	flag = 0;
 	end = NULL;
-	pathlist = map_get(map, "PATH");
-	if (pathlist == NULL)
+	start = map_get(map, "PATH");
+	if (start == NULL)
 		return (NULL);
-	start = pathlist;
-	end = NULL;
 	while (flag == 0)
 	{
-		ft_bzero(path, PATH_MAX);
 		if (end != NULL && *(end + 1) != '\0')
 			start = end + 1;
 		end = ft_strchr(start, ':');
-		if (end)
-			ft_strlcpy(path, start, end - start + 1);
-		else
-		{
-			flag = 1;
-			len = ft_strlen(start);
-			ft_strlcpy(path, start, len + 1);
-		}
-		ft_strlcat(path, "/", PATH_MAX);
-		ft_strlcat(path, file, PATH_MAX);
-		if (access(path, F_OK) == 0)
-		{
-			result = ft_strdup(path);
-			return (result);
-		}
+		if (split_path(file, start, end, &flag))
+			return (split_path(file, start, end, &flag));
 	}
 	return (NULL);
 }
--- utils/error.c
+++ utils/error.c
@@ -1,26 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayu <mayu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 16:04:08 by mayu              #+#    #+#             */
+/*   Updated: 2023/10/19 16:04:22 by mayu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-//内部のエラー、exitする
-void fatal_error(char *msg)
+void	fatal_error(char *msg)
 {
-    ft_putstr_fd("minishell: ", STDERR_FILENO);
-    ft_putendl_fd(msg, STDERR_FILENO);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putendl_fd(msg, STDERR_FILENO);
 	exit(1);
 }
 
-//コマンドのエラー、エラーメッセージ表示
-void error_message(char *command, char *arg, char *msg)
+void	error_message(char *command, char *arg, char *msg)
 {
-    ft_putstr_fd("minishell: ", STDERR_FILENO);
-    if (command)
-    {
-        ft_putstr_fd(command, STDERR_FILENO);
-        ft_putstr_fd(": ", STDERR_FILENO);
-    }
-    if (arg)
-    {
-        ft_putstr_fd(arg, STDERR_FILENO);
-        ft_putstr_fd(": ", STDERR_FILENO);
-    }
-    ft_putendl_fd(msg, STDERR_FILENO);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	if (command)
+	{
+		ft_putstr_fd(command, STDERR_FILENO);
+		ft_putstr_fd(": ", STDERR_FILENO);
+	}
+	if (arg)
+	{
+		ft_putstr_fd(arg, STDERR_FILENO);
+		ft_putstr_fd(": ", STDERR_FILENO);
+	}
+	ft_putendl_fd(msg, STDERR_FILENO);
 }
