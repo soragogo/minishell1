7b8cd3c2be71636ffe738a82243e5ab957403b3a
Mayu Yamada <mr.obake0208@yahoo.com>
2023-10-19 18:48:44 +0900

    signal

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mayu <mayu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:16:54 by mayu              #+#    #+#             */
-/*   Updated: 2023/10/19 17:12:00 by mayu             ###   ########.fr       */
+/*   Updated: 2023/10/19 18:30:04 by mayu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,28 @@
 #include "includes/parser.h"
 #include <stdbool.h>
 
-void	loop_commandline(t_info *info,
+int	loop_commandline(t_info *info,
 	char *command_buf, t_commandset *commands)
 {
-	while (1)
+	ft_signals();
+	command_buf = ft_readline();
+	if (!command_buf)
+		return (0);
+	if (*command_buf == '\0' || only_space(command_buf))
 	{
-		ft_signals();
-		command_buf = ft_readline();
-		if (!command_buf)
-			break ;
-		if (*command_buf == '\0' || only_space(command_buf))
-		{
-			free(command_buf);
-			continue ;
-		}
-		commands = ft_parser(command_buf,
-				&(info->exit_status_log), info->map_head);
-		if (commands == NULL)
-		{
-			free(command_buf);
-			continue ;
-		}
-		info->exit_status_log = handle_command(commands, info);
-		free_before_closing(commands, command_buf);
+		free(command_buf);
+		return (1);
 	}
+	commands = ft_parser(command_buf,
+			&(info->exit_status_log), info->map_head);
+	if (commands == NULL)
+	{
+		free(command_buf);
+		return (1);
+	}
+	info->exit_status_log = handle_command(commands, info);
+	free_before_closing(commands, command_buf);
+	return (1);
 }
 
 int	main(void)
@@ -53,7 +51,8 @@ int	main(void)
 	info.exit_status_log = 0;
 	while (1)
 	{
-		loop_commandline(&info, command_buf, commands);
+		if (loop_commandline(&info, command_buf, commands) == 0)
+			break ;
 	}
 	free_map(&info.map_head);
 	return (0);
