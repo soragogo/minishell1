081dc161b5425e66bff9dbf5fdae1ad503a67a76
mayyamad <mayyamad@student.42tokyo.jp>
2023-10-26 18:05:40 +0900

    signal

--- exec/heredoc.c
+++ exec/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mayu <mayu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mayyamad <mayyamad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:33:32 by mayu              #+#    #+#             */
-/*   Updated: 2023/10/26 14:42:11 by mayu             ###   ########.fr       */
+/*   Updated: 2023/10/26 17:02:12 by mayyamad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,36 +44,19 @@ int	write_to_pipe(int pipefd[2],
 	return (0);
 }
 
+#include <stdio.h>
+#include <readline/readline.h>
+
 void	pipe_handler(int signum)
 {
-	if (signum == SIGQUIT)
-	{
-		rl_redisplay();
-		exit(0);
-	}
 	if (signum == SIGINT)
 	{
-		ft_putchar_fd('\n', STDERR_FILENO);
-		rl_redisplay();
-		ft_putchar_fd('\n', STDERR_FILENO);
+		// ft_putchar_fd('\n', STDERR_FILENO);
+		// rl_redisplay();
+		// ft_putchar_fd('\n', STDERR_FILENO);
+		rl_replace_line("", 0);\
 		exit(0);
 	}
-}
-
-// void	handle_pipe_signals(void)
-// {
-// 	signal(SIGQUIT, pipe_handler);
-// 	signal(SIGINT, pipe_handler);
-// }
-
-void		handle_signal(int signal)
-{
-	extern int		g_status;
-
-	ft_putstr_fd(BACK_CURSOR, STDERR_FILENO);
-	ft_putstr_fd(CLEAR_FROM_CURSOR, STDERR_FILENO);
-	if (signal == SIGINT)
-		ft_putstr_fd("\nminishell>", STDERR_FILENO);
 }
 
 int	heredoc(const char *delimiter, t_info *info)
@@ -85,7 +68,7 @@ int	heredoc(const char *delimiter, t_info *info)
 	while (1)
 	{
 		// handle_pipe_signals();
-		signal(SIGINT, handle_signal);
+		signal(SIGINT, pipe_handler);
 		line = readline("> ");
 		if (line == NULL)
 			break ;
