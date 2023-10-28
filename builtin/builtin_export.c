1f1c6db0244d855719a4db5811718b315e79ed07
Mayu Yamada <mr.obake0208@yahoo.com>
2023-10-29 00:12:28 +0900

    export

--- builtin/builtin_export.c
+++ builtin/builtin_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emukamada <emukamada@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mayu <mayu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:24:13 by mayu              #+#    #+#             */
-/*   Updated: 2023/10/19 02:20:34 by mayu             ###   ########.fr       */
+/*   Updated: 2023/10/29 00:11:57 by mayu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,23 @@ int	display_envlist(t_env **env_head)
 	return (0);
 }
 
+int	is_valid_name(char **name)
+{
+	int	i;
+
+	i = 0;
+	if (name[0][i] == '\0' || ft_isdigit(name[0][i]) == 1)
+		return (1);
+	while (name[0][i] != '\0')
+	{
+		if (name[0][i] == '_' || ft_isalnum(name[0][i]) == 1)
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
 int	ft_export(t_env **map, char **commands)
 {
 	char	*name;
@@ -50,18 +67,18 @@ int	ft_export(t_env **map, char **commands)
 		display_envlist(map);
 		return (0);
 	}
+	name = get_env_name(name, commands[1]);
+	value = get_env_value(value, commands[1]);
+	if (set_env(map, name, value, true) == -1 || is_valid_name(&name) == 1)
+	{
+		error_message("export", NULL, "not a valid identifier");
+		return (1);
+	}
 	if (ft_strchr(commands[1], '=') == NULL)
 	{
 		if (set_env(map, ft_strdup(commands[1]), NULL, true) == -1)
 			return (1);
 		return (0);
-	}
-	name = get_env_name(name, commands[1]);
-	value = get_env_value(value, commands[1]);
-	if (set_env(map, name, value, true) == -1)
-	{
-		error_message("export", NULL, "not a valid identifier");
-		return (1);
 	}
 	return (0);
 }
