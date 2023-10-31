2662a85edd2df1b51fcdd8f3b32d9988562b0ffa
Emu Kamada <es377233@gmail.com>
2023-10-31 14:43:56 +0900

    関係ないとこバグったけど、35945EMPTY治った

--- tokenizer/categorize_tokens.c
+++ tokenizer/categorize_tokens.c
@@ -6,7 +6,7 @@
 /*   By: emukamada <emukamada@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 19:38:41 by ekamada           #+#    #+#             */
-/*   Updated: 2023/10/31 13:02:01 by emukamada        ###   ########.fr       */
+/*   Updated: 2023/10/31 14:41:29 by emukamada        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,12 @@ int	assign_command_type(t_token *token, t_token *prev_token, int *command_flag)
 {
 	if (token->arg[0] == '-' && *command_flag == 1)
 		return (COMMAND_OPTION);
-	else if (token->arg[0] != '\0' && (prev_token == NULL
+	else if (prev_token == NULL
 		|| (*command_flag == 0
 			&& (prev_token->type < REDIRECT_OUT
-				|| prev_token->type > HERE_DOCUMENT))))
+				|| prev_token->type > HERE_DOCUMENT)))
 	{
-		printf("this is command: %s\n", token->arg);
+		// printf("this is command: %s\n", token->arg);
 		*command_flag = 1;
 		return (COMMAND);
 	}
@@ -61,6 +61,7 @@ void	categorize_tokens(t_token *tokens)
 	command_flag = 0;
 	while (tokens[i].arg != NULL)
 	{
+		// printf("tokens[%d].arg: %s, type: %d\n", i, tokens[i].arg, tokens[i].type);
 		type = assign_redirection_type(&tokens[i]);
 		if (type == -1)
 		{
