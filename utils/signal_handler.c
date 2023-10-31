441283de5e5b08f64e09b6812ada210344dde241
Emu Kamada <es377233@gmail.com>
2023-10-31 21:38:04 +0900

    signalちょっと修正'

--- utils/signal_handler.c
+++ utils/signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: emukamada <emukamada@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 17:40:59 by mayu              #+#    #+#             */
-/*   Updated: 2023/10/27 18:01:58 by emukamada        ###   ########.fr       */
+/*   Updated: 2023/10/31 21:37:40 by emukamada        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void	handler(int signum)
 	{
 		rl_on_new_line();
 		ft_putchar_fd('\n', STDERR_FILENO);
+		rl_replace_line("", 0);
 		g_sigstatus = SIGINT;
 		rl_redisplay();
 	}
