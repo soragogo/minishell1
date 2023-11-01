dce70232799f291d086d1493a414b0990687f411
Emu Kamada <es377233@gmail.com>
2023-11-01 10:08:08 +0900

    bug修正

--- tokenizer/parser.c
+++ tokenizer/parser.c
@@ -6,7 +6,7 @@
 /*   By: emukamada <emukamada@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 21:08:51 by emukamada         #+#    #+#             */
-/*   Updated: 2023/10/31 15:54:14 by emukamada        ###   ########.fr       */
+/*   Updated: 2023/11/01 10:06:55 by emukamada        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ t_commandset	*ft_parser(char *buff, int *status, t_env *env_head)
 	t_commandset	*result;
 
 	tokens = ft_tokenizer(buff);
+	categorize_tokens(tokens);
 	if (syntax_error(tokens))
 	{
 		*status = 258;
